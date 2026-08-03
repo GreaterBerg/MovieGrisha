41633ff9c67041ff244c3f285f53ca06381c2290
grisha <derbe.astemir@mail.ru>
2026-08-03 09:41:11 +0300

    feat: обновить навбар

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,10 +6,10 @@ const NavBar = ({isSearch=false}) => {
 
   return (
     <nav>
-        <Link to={"/"} className='logo'>The Movie Tracker</Link>
+        <Link to={"/"} className='logo sharp'>MovieGrisha</Link>
         <ul className='nav-list'>
             { isSearch ? null :
-                <Link to={"/search"} className='item'>Search</Link>
+                <Link to={"/search"} className='item sharp'>Search</Link>
             }
         </ul>
     </nav>
