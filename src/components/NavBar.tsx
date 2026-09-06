4d43f65ecd7e04450e4cc45d3c353c8244604b4f
grisha <derbe.astemir@mail.ru>
2026-09-06 09:42:23 +0300

    feat: обоновить навбар

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,17 @@
 import { Link } from 'react-router-dom'
 import './NavBar.css'
 
+import { Search } from 'lucide-react'
+
+
 
 const NavBar = ({isSearch=false}) => {
 
   return (
     <nav>
-        <Link to={"/"} className='logo sharp'>MovieGrisha</Link>
+        <Link to={"/"} className='logo'>MovieGrisha</Link>
         <ul className='nav-list'>
-            { isSearch ? null :
-                <Link to={"/search"} className='item sharp'>Search</Link>
-            }
+          <Link to={"/search"} className='item'> <Search /> Search</Link>
         </ul>
     </nav>
   )
